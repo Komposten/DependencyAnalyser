f5a08000c27a088e4e5dbc42c308e6954429bb14
Komposten <j.hjelm586@hotmail.com>
2018-12-01 17:40:21 +0200

    Moved all zooming functionality into GraphComponent (from GraphPanel and GraphTabPanel).

--- src/main/java/komposten/analyser/gui/GraphTabPanel.java
+++ src/main/java/komposten/analyser/gui/GraphTabPanel.java
@@ -1,22 +1,15 @@
 package komposten.analyser.gui;
 
-import java.awt.event.KeyEvent;
 
-import javax.swing.ActionMap;
-import javax.swing.InputMap;
 import javax.swing.JTabbedPane;
-import javax.swing.KeyStroke;
 
 import komposten.analyser.gui.backend.Backend;
 import komposten.analyser.gui.backend.Backend.PropertyChangeListener;
-import komposten.analyser.gui.backend.ZoomAction;
-import komposten.analyser.gui.backend.Zoomable;
-import komposten.analyser.gui.views.GraphPanel;
 import komposten.analyser.gui.views.cycles.CyclePanel;
 import komposten.analyser.gui.views.dependencies.DependencyPanel;
 import komposten.analyser.gui.views.files.ClassPanel;
 
-public class GraphTabPanel extends JTabbedPane implements Zoomable
+public class GraphTabPanel extends JTabbedPane
 {
 	private DependencyPanel panelDependencies;
 	private CyclePanel panelCycles;
@@ -34,21 +27,6 @@ public class GraphTabPanel extends JTabbedPane implements Zoomable
 		add("Cycles", panelCycles);
 		add("Classes", panelClasses);
 		
-		createKeyboardShortcuts();
-	}
-
-
-	private void createKeyboardShortcuts()
-	{
-		InputMap inputMap = getInputMap(WHEN_IN_FOCUSED_WINDOW);
-		ActionMap actionMap = getActionMap();
-		
-		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_PLUS, KeyEvent.CTRL_DOWN_MASK, true), "zoomin");
-		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, KeyEvent.CTRL_DOWN_MASK, true), "zoomout");
-		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_0, KeyEvent.CTRL_DOWN_MASK, true), "zoomreset");
-		actionMap.put("zoomin", new ZoomAction(ZoomAction.ZOOM_IN, this));
-		actionMap.put("zoomout", new ZoomAction(ZoomAction.ZOOM_OUT, this));
-		actionMap.put("zoomreset", new ZoomAction(ZoomAction.ZOOM_RESET, this));
 	}
 	
 	
@@ -57,27 +35,6 @@ public class GraphTabPanel extends JTabbedPane implements Zoomable
 		panelDependencies.rebuildGraph();
 		panelCycles.rebuildGraph();
 		panelClasses.rebuildGraph();
-	}
-	
-	
-	@Override
-	public void zoomIn()
-	{
-		((GraphPanel<?, ?>) getSelectedComponent()).zoomIn();
-	}
-	
-	
-	@Override
-	public void zoomOut()
-	{
-		((GraphPanel<?, ?>) getSelectedComponent()).zoomOut();
-	}
-	
-	
-	@Override
-	public void zoomReset()
-	{
-		((GraphPanel<?, ?>) getSelectedComponent()).zoomReset();
 	}
 	
 	
--- src/main/java/komposten/analyser/gui/views/GraphPanel.java
+++ src/main/java/komposten/analyser/gui/views/GraphPanel.java
@@ -13,11 +13,7 @@ import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultListenableGraph;
 
 import com.mxgraph.layout.mxGraphLayout;
-import com.mxgraph.model.mxCell;
 import com.mxgraph.model.mxICell;
-import com.mxgraph.util.mxRectangle;
-import com.mxgraph.view.mxGraph;
-import com.mxgraph.view.mxGraphView;
 
 import komposten.analyser.backend.Edge;
 import komposten.analyser.backend.Vertex;
@@ -69,7 +65,7 @@ public abstract class GraphPanel<V extends Vertex, E extends Edge> extends JPane
 		if (performLayout)
 		{
 			double scale = graphPanel.getGraph().getView().getScale();
-			zoomReset();
+			graphPanel.zoomReset();
 			layoutGraph();
 			graphPanel.getGraph().getView().setScale(scale);;
 		}
@@ -109,126 +105,37 @@ public abstract class GraphPanel<V extends Vertex, E extends Edge> extends JPane
 	
 	public void fitGraphToView()
 	{
-		mxGraphView view = graphPanel.getGraph().getView();
-		int componentWidth = graphPanel.getWidth();
-		int componentHeight = graphPanel.getHeight();
-		
-		mxRectangle graphBounds = getGraphBounds();
-		double viewWidth = graphBounds.getWidth();
-		double viewHeight = graphBounds.getHeight();
-
-		double widthValue = componentWidth/viewWidth;
-		double heightValue = componentHeight/viewHeight;
-		view.setScale(Math.min(widthValue, heightValue) * 0.9);
-		
-		moveGraphIntoView(graphBounds);
+		graphPanel.fitGraphToView();
 	}
 	
 	
 	public void fitGraphToWidth()
 	{
-		mxGraphView view = graphPanel.getGraph().getView();
-		int componentWidth = graphPanel.getWidth();
-
-		mxRectangle graphBounds = getGraphBounds();
-		double viewWidth = graphBounds.getWidth();
-
-		double widthValue = componentWidth/viewWidth;
-		view.setScale(widthValue * 0.9);
-
-		moveGraphIntoView(graphBounds);
+		graphPanel.fitGraphToWidth();
 	}
 	
 	
 	public void fitGraphToHeight()
 	{
-		mxGraphView view = graphPanel.getGraph().getView();
-		int componentHeight = graphPanel.getHeight();
-
-		mxRectangle graphBounds = getGraphBounds();
-		double viewHeight = graphBounds.getHeight();
-
-		double heightValue = componentHeight/viewHeight;
-		view.setScale(heightValue * 0.9);
-
-		moveGraphIntoView(graphBounds);
-	}
-	
-	
-	public void moveGraphIntoView()
-	{
-		moveGraphIntoView(getGraphBounds());
-	}
-	
-	
-	public void moveGraphIntoView(mxRectangle graphBounds)
-	{
-		mxGraph graph = graphPanel.getGraph();
-		
-		Object[] oldSelection = graph.getSelectionCells();
-		
-		graph.selectAll();
-		graph.moveCells(graph.getSelectionCells(), -graphBounds.getX(), -graphBounds.getY());
-		graph.setSelectionCells(oldSelection);
-	}
-	
-	
-	/**
-	 * @return The bounds of graph independent of the current scale.
-	 */
-	private mxRectangle getGraphBounds()
-	{
-		mxGraph graph = graphPanel.getGraph();
-		
-		Object[] oldSelection = graph.getSelectionCells();
-		
-		graph.selectAll();
-		
-		mxCell cell = (mxCell) graph.getSelectionCells()[0];
-		double minX = cell.getGeometry().getX();
-		double minY = cell.getGeometry().getY();
-		double maxX = cell.getGeometry().getX() + cell.getGeometry().getWidth();
-		double maxY = cell.getGeometry().getY() + cell.getGeometry().getHeight();
-		
-		for (Object object : graph.getSelectionCells())
-		{
-			cell = (mxCell)object;
-			
-			if (cell.getGeometry().getX() < minX)
-				minX = cell.getGeometry().getX();
-			if (cell.getGeometry().getX() + cell.getGeometry().getWidth() > maxX)
-				maxX = cell.getGeometry().getX() + cell.getGeometry().getWidth();
-			
-			if (cell.getGeometry().getY() < minY)
-				minY = cell.getGeometry().getY();
-			if (cell.getGeometry().getY() + cell.getGeometry().getHeight() > maxY)
-				maxY = cell.getGeometry().getY() + cell.getGeometry().getHeight();
-		}
-		
-		graph.setSelectionCells(oldSelection);
-		
-		return new mxRectangle(minX, minY, maxX - minX, maxY - minY);
+		graphPanel.fitGraphToHeight();
 	}
 	
 	
 	public void zoomIn()
 	{
-		mxGraphView view = graphPanel.getGraph().getView();
-		view.setScale(view.getScale() * 1.25);
+		graphPanel.zoomIn();
 	}
 	
 	
 	public void zoomOut()
 	{
-		mxGraphView view = graphPanel.getGraph().getView();
-		view.setScale(view.getScale() / 1.25);
+		graphPanel.zoomOut();
 	}
 	
 	
 	public void zoomReset()
 	{
-		mxGraphView view = graphPanel.getGraph().getView();
-		view.setScale(1.25);
+		graphPanel.zoomReset();
 	}
 	
 	
