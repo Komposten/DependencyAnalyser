29d9b214c45b1cd822aaf0dd734a0d451fb1d90c
Komposten <j.hjelm586@hotmail.com>
2018-12-29 14:02:25 +0100

    Updated the histogram visuals
    - Configured the paints used by HistogramChart's BarRenderer (paint, fill paint, outline paint, and highlight paints).
    - Replaced HisogramChart's BarRenderer's GradientXYBarPainter with a StandardXYBarPainter.
    - Added a 2-width outline to HistogramChart's bars.

--- src/main/java/komposten/analyser/gui/views/statistics/HistogramChart.java
+++ src/main/java/komposten/analyser/gui/views/statistics/HistogramChart.java
@@ -1,5 +1,6 @@
 package komposten.analyser.gui.views.statistics;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Paint;
 import java.util.ArrayList;
@@ -11,7 +12,9 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.StandardXYBarPainter;
 import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.chart.util.PaintList;
 import org.jfree.data.xy.AbstractIntervalXYDataset;
 
 public class HistogramChart extends JFreeChart
@@ -162,24 +165,104 @@ public class HistogramChart extends JFreeChart
 	private static class BarRenderer extends XYBarRenderer
 	{
 		private HistogramDataset dataset;
+		private PaintList paintList;
+		private PaintList outlinePaintList;
+		private PaintList fillPaintList;
+		private PaintList highlightPaintList;
+		private PaintList highlightOutlinePaintList;
 		
 		
 		public BarRenderer(HistogramDataset dataset)
 		{
 			this.dataset = dataset;
+			createPaints();
+			
+			setDefaultPaint(paintList.getPaint(0), false);
+			setDefaultFillPaint(fillPaintList.getPaint(0), false);
+			setDefaultOutlinePaint(outlinePaintList.getPaint(0), false);
+			
+			setDrawBarOutline(true);
+			setDefaultOutlineStroke(new BasicStroke(2), false);
+			
+			setBarPainter(new StandardXYBarPainter());
+		}
+
+
+		private void createPaints()
+		{
+			Color[] colours = new Color[]
+					{
+							new Color(248, 118, 109),
+							new Color(216, 144, 0),
+							new Color(163, 165, 0),
+							new Color(57, 182, 0),
+							new Color(0, 191, 125),
+							new Color(0, 191, 196),
+							new Color(0, 176, 246),
+							new Color(149, 144, 255),
+							new Color(231, 107, 243),
+							new Color(255, 98, 188)
+					};
+			Color[] fillColours = new Color[colours.length];
+			Color[] outlineColours = new Color[colours.length];
+			Color[] highlightColours = new Color[colours.length];
+			Color[] highlightOutlineColours = new Color[colours.length];
+			
+			for (int i = 0; i < colours.length; i++)
+			{
+				fillColours[i] = colours[i].brighter();
+				outlineColours[i] = colours[i].darker();
+				highlightColours[i] = fillColours[i].brighter();
+				highlightOutlineColours[i] = new Color(outlineColours[i].getRGB());
+			}
+			
+			paintList = createPaintList(colours);
+			fillPaintList = createPaintList(fillColours);
+			outlinePaintList = createPaintList(outlineColours);
+			highlightPaintList = createPaintList(highlightColours);
+			highlightOutlinePaintList = createPaintList(highlightOutlineColours);
+			
+			for (int i = 0; i < paintList.size(); i++)
+			{
+				setSeriesPaint(i, paintList.getPaint(i), false);
+				setSeriesFillPaint(i, fillPaintList.getPaint(i), false);
+				setSeriesOutlinePaint(i, outlinePaintList.getPaint(i), false);
+			}
+		}
+
+
+		private PaintList createPaintList(Color[] colours)
+		{
+			PaintList list = new PaintList();
+			for (int i = 0; i < colours.length; i++)
+			{
+				list.setPaint(i, colours[i]);
+			}
+			return list;
 		}
 		
 		
 		@Override
 		public Paint getItemPaint(int row, int column)
 		{
-			//CURRENT Create a complete set of colours to use for multiple series (with highlights)
-			//				to use instead of the default!
-			int highlightedBar = dataset.dataSeries.get(row).getHighlightedXValue();
-			
-			if (highlightedBar == column)
-				return Color.GREEN;
+			if (isHighlighted(row, column))
+				return highlightPaintList.getPaint(row);
 			return super.getItemPaint(row, column);
+		}
+		
+		
+		@Override
+		public Paint getItemOutlinePaint(int row, int column)
+		{
+			if (isHighlighted(row, column))
+				return highlightOutlinePaintList.getPaint(row);
+			return super.getItemOutlinePaint(row, column);
+		}
+		
+		
+		private boolean isHighlighted(int row, int column)
+		{
+			return dataset.dataSeries.get(row).getHighlightedXValue() == column;
 		}
 	}
 }
